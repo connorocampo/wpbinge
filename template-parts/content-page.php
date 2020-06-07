529564df9089a2e790a6fa539dc1918ada97bbe8
Connor Ocampo <connorocampo@gmail.com>
2020-06-07 18:57:30 -0400

    add featured image page support

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,6 +1,13 @@
 <section class="page-container">
     <div class="page">
 
+    <img
+        src="<?php the_post_thumbnail_url('thumbnail'); ?>"
+        alt=""
+        class="featured-image"
+    />
+    
+
     <h1>
         <?php the_title(); ?>
     </h1>
