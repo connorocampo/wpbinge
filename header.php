74d6007b149019d07dcebcc87c923e370805a02c
Connor Ocampo <connorocampo@gmail.com>
2020-06-11 10:44:07 -0400

    Update to Version: 1.0.7

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <!-- Meta -->
-    <title>WP Binge | Learn WordPress</title>
+    <title><?php the_title(); ?> | Learn WordPress</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
